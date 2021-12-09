2aaaed0aa5984ad168760429d5472a9b783ce35e
Théo R <31210187+Muskya@users.noreply.github.com>
2021-12-09 18:37:16 +0100

    jQuery 3.6.0, intro img fade on click function

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,26 @@
-// Changes displayed portrait on intro page
-let introImg = document.querySelector(".home-intro img");
-function introImgChangeImg() {
-    if (introImg.getAttribute("src") === "resources/portrait2.jpg") {
-        
-        introImg.setAttribute("src", "resources/portrait1.jpg");
-    } else {
-        introImg.setAttribute("src", "resources/portrait2.jpg");
-    }
-}
+// Checks for jQuery presence
+if (jQuery) console.log("jQuery loaded");
+else console.log("jQuery not loaded");
 
-// Ne pas ajouter les () d'une fonction dans un addEventListener !!
-introImg.addEventListener("click", introImgChangeImg);
+// Changes displayed portrait on intro page (static, no transition)
+// let introImg = document.querySelector(".home-intro img");
+// function introImgChangeImg() {
+//     if (introImg.getAttribute("src") === "resources/portrait2.jpg") {
+//         introImg.setAttribute("src", "resources/portrait1.jpg");
+//     } else {
+//         introImg.setAttribute("src", "resources/portrait2.jpg");
+//     }
+// }
+// // Ne pas ajouter les () d'une fonction dans un addEventListener !!
+// introImg.addEventListener("click", introImgChangeImg);
+
+// jQuery fadeIn() fadeOut() essai (switch d'image à l'intro)
+// .fadeOut|In( [duration (default 400ms)], []) 
+$(document.querySelector(".home-intro img")).click(function() {
+    $(".home-intro img").fadeOut(350, function() {
+        if ($(this).attr("src") === "resources/portrait2.jpg")
+            $(this).attr("src", "resources/portrait1.jpg").fadeIn();
+        else 
+             $(this).attr("src", "resources/portrait2.jpg").fadeIn();
+    });
+});
