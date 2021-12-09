286c872cdacd8c5baf07db491d46e2568e107e00
Théo R <31210187+Muskya@users.noreply.github.com>
2021-12-09 17:57:11 +0100

    img switch click, separated practice/main .js

--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,13 @@
-/* document.addEventListener("DOMContentLoaded"..) :
-Will wait for the HTML document to be fully parsed before
-executing any Javascript the anonymous function contains.
-It is not needed if "defer" attribute is mentionned when linking .js files */
-document.addEventListener("DOMContentLoaded", () => {
-    // code after HTML fully parsed and loaded ..
-});
-
-/* SOME PRACTICE : */
-
-// Click counter
-let page = document.querySelector("html");
-let myH3Counter = document.querySelector("h3");
-let clickCounter = 0;
-page.onclick = function() {
-    clickCounter++;
-    myH3Counter.textContent = "This counter is incremented by JS: " + clickCounter;
-    console.log(clickCounter);
-};
-
-// Paragraphs' text update on click
-let paragraphs = document.querySelectorAll("p");
-for (const p of paragraphs) {
-    p.onclick = function() {
-        p.textContent = "Just clicked on a paragraph !";
-    };
+// Changes displayed portrait on intro page
+let introImg = document.querySelector(".home-intro img");
+function introImgChangeImg() {
+    if (introImg.getAttribute("src") === "resources/portrait2.jpg") {
+        
+        introImg.setAttribute("src", "resources/portrait1.jpg");
+    } else {
+        introImg.setAttribute("src", "resources/portrait2.jpg");
+    }
 }
 
-
-
+// Ne pas ajouter les () d'une fonction dans un addEventListener !!
+introImg.addEventListener("click", introImgChangeImg);
