1d43b1d3fc3bba7ebb15f707d08f4e4f58d62d8f
Théo R <31210187+Muskya@users.noreply.github.com>
2021-12-17 12:29:07 +0100

    splitted css files for each page

--- javascript/main.js
+++ javascript/main.js
@@ -2,18 +2,6 @@
 if (jQuery) console.log("jQuery loaded");
 else console.log("jQuery not loaded");
 
-// Changes displayed portrait on intro page (static, no transition)
-// let introImg = document.querySelector(".home-intro img");
-// function introImgChangeImg() {
-//     if (introImg.getAttribute("src") === "resources/portrait2.jpg") {
-//         introImg.setAttribute("src", "resources/portrait1.jpg");
-//     } else {
-//         introImg.setAttribute("src", "resources/portrait2.jpg");
-//     }
-// }
-// // Ne pas ajouter les () d'une fonction dans un addEventListener !!
-// introImg.addEventListener("click", introImgChangeImg);
-
 // jQuery fadeIn() fadeOut() essai (switch d'image à l'intro)
 // .fadeOut|In( [duration (default 400ms)], []) 
 $(document.querySelector(".home-intro img")).click(function() {
--- javascript/practice.js
+++ javascript/practice.js
@@ -1,30 +1,11 @@
-/* document.addEventListener("DOMContentLoaded"..) :
-Will wait for the HTML document to be fully parsed before
-executing any Javascript the anonymous function contains.
-It is not needed if "defer" attribute is mentionned when linking .js files */
-document.addEventListener("DOMContentLoaded", () => {
-    // code after HTML fully parsed and loaded ..
-});
-
-/* SOME PRACTICE : */
-
 // Click counter
 let page = document.querySelector("html");
-let myH3Counter = document.querySelector("h3");
+let myH3Counter = document.querySelector("h3.click-counter");
 let clickCounter = 0;
 page.onclick = function() {
     clickCounter++;
     myH3Counter.textContent = "This counter is incremented by JS: " + clickCounter;
-    // console.log(clickCounter);
 };
-
-// Paragraphs' text update on click
-let paragraphs = document.querySelectorAll("p");
-for (const p of paragraphs) {
-    p.onclick = function() {
-        p.textContent = "Just clicked on a paragraph !";
-    };
-}
 
 
 
